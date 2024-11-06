3c8171114122a123a2abe0205b6356dd8ac23aa8
Matias Ilola <matias.ilola@helsinki.fi>
2024-11-06 14:27:09 +0200

    muutos

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -12,4 +12,4 @@ print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branch
 print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
 
 logger("lopetetaan ohjelma")
-print("goodbye!")
+print("goodbye!")#muutos
